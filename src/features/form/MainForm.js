cc1f585703e105ee4f2bbcbf4e7d0de913cae5be
Ruoqian <u6862849@anu.edu.au>
2021-08-08 20:29:23 +1000

    added fields in geometry

--- src/features/form/MainForm.js
+++ src/features/form/MainForm.js
@@ -1,24 +1,54 @@
 import { Formik, Form, Field, FieldArray } from 'formik'
 import React, { Fragment } from 'react'
+import Geometry, { geometryInitialValues } from './components/Geometry'
 import Material, { materialInitialValues } from './components/Material'
 import Surface, { surfaceInitialValues } from './components/Surface'
 
 export const formInitialValues = {
     blankSource: '',
     material: materialInitialValues,
+    geometry: geometryInitialValues,
     surface: [surfaceInitialValues]
 }
 
+export const flattenObject = (obj) => {
+    const flattened = {}
+  
+    Object.keys(obj).forEach((key) => {
+      if (typeof obj[key] === 'object' && obj[key] !== null) {
+        Object.assign(flattened, flattenObject(obj[key]))
+      } else {
+        flattened[key] = obj[key]
+      }
+    })
+    return flattened
+  }
+
 export default function MainForm() {
+
+
+      const getChangedValues = (values, initialValues) => {
+        const flattenedValues = flattenObject(values)
+        const flattenInitial = flattenObject(initialValues)
+        return Object.entries(flattenedValues).reduce((acc, [key, value]) => {
+            const hasChanged = flattenInitial[key] !== value
+            if (hasChanged)
+                acc[key] = value
+            return acc
+        }, {})
+    }
+
     return (
         <Fragment>
             <h1>Request Form</h1>
             <Formik
                 initialValues={formInitialValues}
-                onSubmit={async (values) => {
-                    await new Promise((r) => setTimeout(r, 500));
-                    alert(JSON.stringify(values, null, 2));
-                }}
+                onSubmit={
+                    async (values) => {
+                        console.log(formInitialValues)
+                        await new Promise((r) => setTimeout(r, 500));
+                        alert(JSON.stringify(getChangedValues(values, formInitialValues), null, 2));
+                    }}
                 >
                 {({values, handleChange, handleSubmit}) => (
                     <Form>
@@ -52,9 +82,14 @@ export default function MainForm() {
                                                 <option value='Custom supplied'>Custom supplied</option>
                                             </Field>
                                             <Material 
-                                            serviceType='spdt' 
-                                            handleChange={handleChange} 
-                                            materialValues={values.material}
+                                                serviceType='spdt' 
+                                                handleChange={handleChange} 
+                                                materialValues={values.material}
+                                            />
+                                            <Geometry
+                                                handleChange={handleChange}
+                                                geometryValues={values.geometry}
+                                                blankSource={values.blankSource}
                                             />
                                             <FieldArray
                                                 name='surface'
@@ -63,6 +98,7 @@ export default function MainForm() {
                                                     <div>
                                                         {values.surface.map((side, index) => (
                                                             <Surface 
+                                                                key = {index}
                                                                 index={index} 
                                                                 handleChange={handleChange}
                                                                 surfaceValues={values.surface[index]}
--- src/features/form/components/Geometry.js
+++ src/features/form/components/Geometry.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Field } from 'formik'
+import React, { Fragment } from 'react'
+import { MyTextInput } from '../fields/MyTextInput'
 
-export default function Geometry() {
+export const geometryInitialValues = {
+    geometryType: '',
+    majorDiameter: '',
+    minorDiamter: '',
+    thickness: '',
+    wedge: '',
+    chamferx: '',
+    chamfery: ''
+}
+
+export default function Geometry({geometryValues, handleChange, blankSource}) {
     return (
         <div>
-            
+            <label>
+                Geometry:
+                <Field as='select' name='geometry.geometryType' >
+                    <option value='N/A'>Please select</option>
+                    <option value='oval'>Oval</option>
+                    <option value='rectangle'>Rectangle</option>
+                    <option value='regular polygon'>Regular Polygon</option>
+                    <option value='other'>Other</option>
+                </Field>
+            </label>
+            {geometryValues.geometryType === 'oval' && (
+                <Fragment>
+                    <MyTextInput label='Major Diameter (mm):' name='geometry.majorDiameter' onChange={handleChange} />
+                    <MyTextInput label='Minor Diameter (mm):' name='geometry.minorDiameter' onChange={handleChange} />
+                    <MyTextInput label='Thickness (mm):' name='geometry.thickness' onChange={handleChange} />
+                    <MyTextInput label='Wedge (arcsecond):' name='geometry.wedge' onChange={handleChange} />
+                    <MyTextInput label='Chamfer x:' name='geometry.chamferx' onChange={handleChange} />
+                    <MyTextInput label='Chamfer y:' name='geometry.chamfery' onChange={handleChange} />
+                </Fragment>
+            )}
         </div>
     )
 }
