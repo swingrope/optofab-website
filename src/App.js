7ec2acec15d5e319384dc68c5b6ce5fa5de5a972
Ruoqian <u6862849@anu.edu.au>
2021-08-29 13:44:50 +1000

    finished submit buttons

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import React, { useState } from 'react';
 import Feedback from "./features/form/Feedback";
 import Modify from "./features/form/Modify";
 import Track from "./features/form/Track";
+import CustomerInfo from './features/form/components/CustomerInfo';
 
 function App() {
 
-  const [part, setPart] = useState(0)
+  const [part, setPart] = useState(1)
 
   return (
     <Router>
       <Switch>
         <Route exact path='/'>
           <MainForm part={part} setPart={setPart}/>
+        </Route>
+        <Route path='/customer'>
+          <CustomerInfo part={part}/>
         </Route>
         <Route path='/error'>
             Error
