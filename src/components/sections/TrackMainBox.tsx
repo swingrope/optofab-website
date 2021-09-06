f85eaa73b0dde3f81ab92369e5cd5d96b1a15fc2
tian <tianwu@outlook.com>
2021-09-06 18:58:48 +1000

    refactor track page

--- src/components/sections/TrackMainBox.tsx
+++ src/components/sections/TrackMainBox.tsx
@@ -5,34 +5,18 @@ import SearchForm from "../forms/SearchForm";
 
 const TrackFormWrapper = () => {
   return (
-    <Wrapper>
-      <ContentWrapper>
-        <FormWrapper>
-          <SearchWrapper>
-            <SearchForm
-              icon="/images/smallicons/help.svg"
-              placeholder="Order No."
-            />
-          </SearchWrapper>
-        </FormWrapper>
-      </ContentWrapper>
-    </Wrapper>
+    <FormWrapper>
+      <SearchWrapper>
+        <SearchForm
+          icon="/images/smallicons/help.svg"
+          placeholder="Order No."
+        />
+      </SearchWrapper>
+    </FormWrapper>
   );
 };
 
 export default TrackFormWrapper;
-
-const Wrapper = styled.div`
-  background: linear-gradient(115.82deg, #00486f 0%, #eb7776 93.65%);
-`;
-
-const ContentWrapper = styled.div`
-  position: relative;
-  max-width: 1234px;
-  height: 1080px;
-  margin: 0 auto;
-  padding: 200px 30px;
-`;
 
 const FormWrapper = styled.div`
   /* Background Blur */
--- src/pages/track.tsx
+++ src/pages/track.tsx
@@ -1,13 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 import Layout from "../components/layout/layout";
 import TrackMainBox from "../components/sections/TrackMainBox";
 
 const TrackPage = () => (
-  <>
-    <Layout>
-      <TrackMainBox />
-    </Layout>
-  </>
+  <Layout>
+    <Wrapper>
+      <ContentWrapper>
+        <TrackMainBox />
+      </ContentWrapper>
+    </Wrapper>
+  </Layout>
 );
 
 export default TrackPage;
+
+const Wrapper = styled.div`
+  background: linear-gradient(115.82deg, #00486f 0%, #eb7776 93.65%);
+`;
+
+const ContentWrapper = styled.div`
+  max-width: 1234px;
+  height: 1080px;
+  margin: 0 auto;
+  padding: 200px 30px;
+`;
