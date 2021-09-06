f17677def1ae857a69647d03e270a2994b546d1d
tian <tianwu@outlook.com>
2021-09-06 16:50:26 +1000

    refactor track page

--- src/pages/track.tsx
+++ src/pages/track.tsx
@@ -1,24 +1,13 @@
-import React from "react"
-import Layout from "../components/layout/layout"
-import styled from "styled-components"
+import React from "react";
+import Layout from "../components/layout/layout";
+import TrackMainBox from "../components/sections/TrackMainBox";
 
-const RequestPage = () => (
+const TrackPage = () => (
   <>
     <Layout>
-      <Wrapper>
-        <ContentWrapper></ContentWrapper>
-      </Wrapper>
+      <TrackMainBox />
     </Layout>
   </>
-)
+);
 
-export default RequestPage
-
-const Wrapper = styled.div`
-  background: linear-gradient(115.82deg, #00486f 0%, #eb7776 93.65%);
-`
-const ContentWrapper = styled.div`
-  max-width: 1234px;
-  margin: 0 auto;
-  padding: 200px 30px;
-`
+export default TrackPage;
