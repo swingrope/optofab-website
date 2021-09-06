a4b4ea68f6903a76e7f22f1752a5422624c80310
tian <tianwu@outlook.com>
2021-09-06 16:49:46 +1000

    restyle basic button

--- src/components/buttons/ButtonComponent.tsx
+++ src/components/buttons/ButtonComponent.tsx
@@ -1,31 +1,35 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
-const ButtonComponent = () => {
+const ButtonComponent = (props) => {
+  const { title } = props;
   return (
     <div>
-      <Button>Click me!</Button>
-      <Button>Click me!</Button>
+      <Button>{title}</Button>
     </div>
-  )
-}
+  );
+};
 
-export default ButtonComponent
+export default ButtonComponent;
 
 const Button = styled.button`
   /* display: block; */
-  margin: 10px;
+  margin: 0px 16px 0 16px;
   padding: 12px 0;
 
   /* position: static; */
-  width: 200px;
+  width: 160px;
+  height: 44px;
 
   background: linear-gradient(270deg, #d90068 0%, rgba(0, 28, 129, 0.9) 100%);
-  box-shadow: 0px 20px 40px rgba(147, 231, 221, 0.3);
+  box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.15);
+  backdrop-filter: blur(40px);
+  /* Note: backdrop-filter has minimal browser support */
+
   border-radius: 30px;
   border: none;
 
   color: white;
   font-weight: bold;
   font-family: Segoe UI, sans-serif;
-`
+`;
