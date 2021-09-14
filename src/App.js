2a7cd9e46e646cdb089c349a825025df956a34c9
tian <tianwu@outlook.com>
2021-09-14 22:27:48 +1000

    reroute webpages

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
-import './App.css';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import MainForm from './features/form/MainForm';
-import React, { useState } from 'react';
+import "./App.css";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  NavLink,
+} from "react-router-dom";
+import MainForm from "./features/form/MainForm";
+import React, { useState } from "react";
 import Feedback from "./features/form/Feedback";
 import Modify from "./features/form/Modify";
 import Track from "./features/form/Track";
-import CustomerInfo from './features/form/components/CustomerInfo';
+import CustomerInfo from "./features/form/components/CustomerInfo";
 
 function App() {
-
-  const [part, setPart] = useState(1)
+  const [part, setPart] = useState(1);
 
   return (
     <Router>
       <Switch>
-        <Route exact path='/'>
-          <MainForm part={part} setPart={setPart}/>
+        <Route exact path="/">
+          <div>
+            <NavLink to="/request">Request</NavLink>
+          </div>
         </Route>
-        <Route path='/customer'>
-          <CustomerInfo part={part}/>
+        <Route path="/request">
+          <MainForm part={part} setPart={setPart} />
         </Route>
-        <Route path='/error'>
-            Error
+        <Route path="/error">Error</Route>
+        <Route path="/feedback">
+          <Feedback />
         </Route>
-          <Route path='/feedback'>
-              <Feedback/>
-          </Route>
-          <Route path='/modify'>
-              <Modify/>
-          </Route>
-          <Route path='/track'>
-              <Track/>
-          </Route>
+        <Route path="/modify">
+          <Modify />
+        </Route>
+        <Route path="/customer">
+          <CustomerInfo part={part} />
+        </Route>
+        <Route path="/track">
+          <Track />
+        </Route>
       </Switch>
     </Router>
   );
