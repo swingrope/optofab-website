1a76c37458f8090b5f2aca1e8ce40b0fdeeb7b53
tian <tianwu@outlook.com>
2021-09-14 23:42:09 +1000

    tune homepage

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "./components/layout/layout";
+import HeroSection from "./components/sections/HeroSection";
+const HomePage = () => (
+  <>
+    <Layout>
+      <HeroSection />
+    </Layout>
+  </>
+);
+
+export default HomePage;
