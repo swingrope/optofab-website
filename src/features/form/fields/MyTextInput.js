506f516e4fb7e5dd7e19fd064a55e82cc156a5b1
tian <tianwu@outlook.com>
2021-09-15 01:10:19 +1000

    style input textbox

--- src/features/form/fields/MyTextInput.js
+++ src/features/form/fields/MyTextInput.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { useField } from 'formik';
+import React from "react";
+import styled from "styled-components";
+import { useField } from "formik";
 
 export const MyTextInput = ({ label, labelClass, br = true, ...props }) => {
   const [field, meta] = useField(props);
@@ -7,10 +8,10 @@ export const MyTextInput = ({ label, labelClass, br = true, ...props }) => {
     <>
       <label className={labelClass}>
         {label}
-        <input {...field} {...props} />
-        {br && (
-          <br />
-          )}
+        {/* <input {...field} {...props} /> */}
+        <Input {...field} {...props} placeholder="Type in order number here" />
+
+        {br && <br />}
       </label>
       {meta.touched && meta.error ? (
         <div className="error">{meta.error}</div>
@@ -18,3 +19,15 @@ export const MyTextInput = ({ label, labelClass, br = true, ...props }) => {
     </>
   );
 };
+
+const Input = styled.input`
+  font-size: 15px;
+  line-height: 130%;
+  text-align: left;
+  color: rgba(255, 255, 255, 0.8);
+  background: transparent;
+  /* get rid of input border */
+  border: 0;
+  /* no border highlight when typing */
+  outline: none;
+`;
