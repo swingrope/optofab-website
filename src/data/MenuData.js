90231ea3643e02bd13329524ea42fdfc6be06ece
tian <tianwu@outlook.com>
2021-09-14 23:41:26 +1000

    use local data array for now

---
+++ src/data/MenuData.js
@@ -0,0 +1,31 @@
+export const MenuData = [
+  { title: "Home", icon: "/images/smallicons/home.svg", link: "/" },
+  {
+    title: "Request a Part",
+    icon: "/images/smallicons/book.svg",
+    link: "/request",
+  },
+  {
+    title: "Track Order",
+    icon: "/images/smallicons/code.svg",
+    link: "/track",
+  },
+  {
+    title: "Modify Order",
+    icon: "/images/smallicons/pen.svg",
+    link: "/modify",
+  },
+  {
+    title: "Feedback",
+    icon: "/images/smallicons/team.svg",
+    link: "/feedback",
+  },
+];
+
+export const tooltipData = [
+  {
+    title: "Feedback",
+    icon: "/images/smallicons/team.svg",
+    link: "/feedback",
+  },
+];
