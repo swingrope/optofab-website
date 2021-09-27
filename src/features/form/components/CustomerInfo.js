18949bea4ac5bb902bf955571b0dba1cbd616487
tian <tianwu@outlook.com>
2021-09-27 16:24:06 +1000

    add customer info page styles

--- src/features/form/components/CustomerInfo.js
+++ src/features/form/components/CustomerInfo.js
@@ -1,114 +1,297 @@
-import { Field, Form, Formik } from 'formik'
-import React, { useState } from 'react'
-import { MyTextInput } from '../fields/MyTextInput';
+import { Field, Form, Formik } from "formik";
+import React, { useState } from "react";
+import styled from "styled-components";
 import ReCAPTCHA from "react-google-recaptcha";
 
+import Layout from "../../../components/layout/layout";
+import { MyTextInputSim } from "../fields/MyTextInputSim";
+import { Caption2, H1, H3 } from "../../../components/styles/TextStyles";
+
 export const customerInfoInitialValues = {
-    sameAsAbove: false
-}
+  sameAsAbove: false,
+};
 
-export default function CustomerInfo({part}) {
+export default function CustomerInfo({ part }) {
+  const baseURL = "../../api/OrderRequest.php";
+  //const baseURL = 'http://localhost:8080/comp8715/optofab-website/src/api/OrderRequest.php'
+  const [verified, setVerified] = useState(false);
 
-    const baseURL = '../../api/OrderRequest.php'
-    //const baseURL = 'http://localhost:8080/comp8715/optofab-website/src/api/OrderRequest.php'
-    const [verified, setVerified] = useState(false)
+  function fillBilling(values, setFieldValue) {
+    values.sameAsAbove = !values.sameAsAbove;
 
-    function fillBilling(values, setFieldValue) {
-        
-        values.sameAsAbove = !values.sameAsAbove
-
-        if(values.sameAsAbove) {
-            for (const [key, value] of Object.entries(values)) {
-                if(key.endsWith('Shipping')) {
-                    let name = key.replace('Shipping', 'Billing')
-                    setFieldValue(name, value)
-                }
-            }
+    if (values.sameAsAbove) {
+      for (const [key, value] of Object.entries(values)) {
+        if (key.endsWith("Shipping")) {
+          let name = key.replace("Shipping", "Billing");
+          setFieldValue(name, value);
         }
-        else {
-            for (const [key] of Object.entries(values)) {
-                if(key.endsWith('Billing')) {
-                    setFieldValue(key, '')
-                }
-            }
+      }
+    } else {
+      for (const [key] of Object.entries(values)) {
+        if (key.endsWith("Billing")) {
+          setFieldValue(key, "");
         }
+      }
+    }
+  }
+
+  function handleSubmitForm(values) {
+    if (!verified) return;
+
+    let data = { customerInfo: values };
+
+    for (let i = 1; i < part; i++) {
+      let itemName = `part${i}`;
+      let item = window.localStorage.getItem(itemName);
+      data[itemName] = JSON.parse(item);
     }
 
-    function handleSubmitForm(values) {
+    console.log(data);
+    // next is ajax request
+    fetch(baseURL, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 
-        if (!verified)
-            return
-
-        let data = { customerInfo: values }
-    
-        for (let i=1; i<part; i++) {
-            let itemName = `part${i}`
-            let item = window.localStorage.getItem(itemName)
-            data[itemName] = JSON.parse(item)
-        }
-
-        console.log(data)
-        // next is ajax request
-        fetch(baseURL, {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-    }
-    
-    return (
-        <Formik
+  return (
+    <Layout>
+      <Wrapper>
+        <ContentWrapper>
+          <Title>Customer Information</Title>
+          <Formik
             initialValues={customerInfoInitialValues}
             onSubmit={(values, actions) => {
-                setTimeout(() => {
-                handleSubmitForm(values)
+              setTimeout(() => {
+                handleSubmitForm(values);
                 actions.setSubmitting(false);
-                }, 1000);
-            }}>
-            {({handleChange, values, setFieldValue, isSubmitting}) => (
+              }, 1000);
+            }}
+          >
+            {({ handleChange, values, setFieldValue, isSubmitting }) => (
+              <FormWrapper>
                 <Form>
-                    <h1>Customer Information</h1>
-                    <MyTextInput name='firstName' label='First Name' onChange={handleChange} />
-                    <MyTextInput name='lastName' label='Last Name' onChange={handleChange} />
-                    <MyTextInput name='companyName' label='Company Name' onChange={handleChange} />
-                    <MyTextInput name='email' label='Email' onChange={handleChange} />
-                    <MyTextInput name='phoneNumber' label='Company Name' onChange={handleChange} />
-                    
-                    <h2>Shipping Address</h2>
-                    <MyTextInput name='firstNameShipping' label='First Name' onChange={handleChange} />
-                    <MyTextInput name='lastNameShipping' label='Last Name' onChange={handleChange} />
-                    <MyTextInput name='companyNameShipping' label='Company Name' onChange={handleChange} />
-                    <MyTextInput name='address1Shipping' label='Address' onChange={handleChange} />
-                    <MyTextInput name='address2Shipping' label='Address2' onChange={handleChange} />
-                    <MyTextInput name='countryShipping' label='Country' onChange={handleChange} />
-                    <MyTextInput name='stateShipping' label='State' onChange={handleChange} />
-                    <MyTextInput name='cityShipping' label='City' onChange={handleChange} />
-                    <MyTextInput name='postcodeShipping' label='PostCode' onChange={handleChange} />
-                    
-                    <label>
-                        same as above?
-                        <Field name='sameAsAbove' type='checkbox' onClick={() => fillBilling(values, setFieldValue)}/>
-                    </label>
-                    <h2>Billing Address</h2>
-                    <MyTextInput name='firstNameBilling' label='First Name' onChange={handleChange} />
-                    <MyTextInput name='lastNameBilling' label='Last Name' onChange={handleChange} />
-                    <MyTextInput name='companyNameBilling' label='Company Name' onChange={handleChange} />
-                    <MyTextInput name='address1Billing' label='Address' onChange={handleChange} />
-                    <MyTextInput name='address2Billing' label='Address2' onChange={handleChange} />
-                    <MyTextInput name='countryBilling' label='Country' onChange={handleChange} />
-                    <MyTextInput name='stateBilling' label='State' onChange={handleChange} />
-                    <MyTextInput name='cityBilling' label='City' onChange={handleChange} />
-                    <MyTextInput name='postcodeBilling' label='PostCode' onChange={handleChange} />
+                  <MyTextInputSim
+                    name="firstName"
+                    label="First Name"
+                    onChange={handleChange}
+                  />
+                  <MyTextInputSim
+                    name="lastName"
+                    label="Last Name"
+                    onChange={handleChange}
+                  />
+                  <MyTextInputSim
+                    name="companyName"
+                    label="Company Name"
+                    onChange={handleChange}
+                  />
+                  <MyTextInputSim
+                    name="email"
+                    label="Email"
+                    onChange={handleChange}
+                  />
+                  <MyTextInputSim
+                    name="phoneNumber"
+                    label="Company Name"
+                    onChange={handleChange}
+                  />
 
-                    <ReCAPTCHA
-                        sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
-                        onChange={() => setVerified(true)}
+                  <SectionTitle>Shipping Address</SectionTitle>
+                  <MyTextInputSim
+                    name="firstNameShipping"
+                    label="First Name"
+                    onChange={handleChange}
+                  />
+                  <MyTextInputSim
+                    name="lastNameShipping"
+                    label="Last Name"
+                    onChange={handleChange}
+                  />
+                  <MyTextInputSim
+                    name="companyNameShipping"
+                    label="Company Name"
+                    onChange={handleChange}
+                  />
+                  <MyTextInputSim
+                    name="address1Shipping"
+                    label="Address"
+                    onChange={handleChange}
+                    placeholder="Address Line 1"
+                  />
+                  <MyTextInputSim
+                    name="address2Shipping"
+                    label="Address2"
+                    onChange={handleChange}
+                    placeholder="Address Line 2"
+                  />
+                  <MyTextInputSim
+                    name="countryShipping"
+                    label="Country"
+                    onChange={handleChange}
+                  />
+                  <MyTextInputSim
+                    name="stateShipping"
+                    label="State"
+                    onChange={handleChange}
+                  />
+                  <MyTextInputSim
+                    name="cityShipping"
+                    label="City"
+                    onChange={handleChange}
+                  />
+                  <MyTextInputSim
+                    name="postcodeShipping"
+                    label="PostCode"
+                    onChange={handleChange}
+                  />
+
+                  <SectionTitle>Billing Address</SectionTitle>
+                  <label>
+                    <PromptWrapper>Same as above?</PromptWrapper>
+                    <Field
+                      name="sameAsAbove"
+                      type="checkbox"
+                      onClick={() => fillBilling(values, setFieldValue)}
                     />
-                    <button type='submit' disabled={isSubmitting}>Submit</button>
+                  </label>
+                  <MyTextInputSim
+                    name="firstNameBilling"
+                    label="First Name"
+                    onChange={handleChange}
+                  />
+                  <MyTextInputSim
+                    name="lastNameBilling"
+                    label="Last Name"
+                    onChange={handleChange}
+                  />
+                  <MyTextInputSim
+                    name="companyNameBilling"
+                    label="Company Name"
+                    onChange={handleChange}
+                  />
+                  <MyTextInputSim
+                    name="address1Billing"
+                    label="Address"
+                    onChange={handleChange}
+                    placeholder="Address Line 1"
+                  />
+                  <MyTextInputSim
+                    name="address2Billing"
+                    label="Address2"
+                    onChange={handleChange}
+                    placeholder="Address Line 2"
+                  />
+                  <MyTextInputSim
+                    name="countryBilling"
+                    label="Country"
+                    onChange={handleChange}
+                  />
+                  <MyTextInputSim
+                    name="stateBilling"
+                    label="State"
+                    onChange={handleChange}
+                  />
+                  <MyTextInputSim
+                    name="cityBilling"
+                    label="City"
+                    onChange={handleChange}
+                  />
+                  <MyTextInputSim
+                    name="postcodeBilling"
+                    label="PostCode"
+                    onChange={handleChange}
+                  />
+
+                  <CaptchaWrapper>
+                    <ReCAPTCHA
+                      sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+                      onChange={() => setVerified(true)}
+                    />
+                    <FinalSubmitButton type="submit" disabled={isSubmitting}>
+                      Submit
+                    </FinalSubmitButton>
+                  </CaptchaWrapper>
                 </Form>
+              </FormWrapper>
             )}
-        </Formik>
-    )
+          </Formik>
+        </ContentWrapper>
+      </Wrapper>
+    </Layout>
+  );
 }
+
+const Wrapper = styled.div`
+  background: linear-gradient(135deg, #1a2151 10%, #eb7776 90%);
+  display: grid;
+  place-items: center;
+`;
+
+const ContentWrapper = styled.div`
+  position: relative;
+  display: grid;
+  gap: 30px;
+  justify-items: center;
+  max-width: 1500px;
+  height: 100%;
+  padding: 160px 30px;
+`;
+
+const FormWrapper = styled.div`
+  background: rgba(255, 255, 255, 0.2);
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1);
+  border-radius: 20px;
+  padding: 30px;
+`;
+
+const Title = styled(H1)`
+  /* color: rgba(255, 255, 255, 0.8); */
+  color: #be1e69;
+  text-shadow: 0px 10px 40px rgba(0, 0, 0, 0.3);
+`;
+
+const SectionTitle = styled(H3)`
+  color: #0f044c;
+  opacity: 0.8;
+  margin: 30px 0;
+`;
+
+const PromptWrapper = styled(Caption2)`
+  display: inline;
+  color: #a12568;
+`;
+
+const FinalSubmitButton = styled.button`
+  margin: 10px;
+  padding: 12px 24px;
+  width: minmax(200px, 360px);
+  color: white;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 130%;
+  text-transform: uppercase;
+  background: linear-gradient(135deg, #d90068 10%, rgba(0, 28, 129, 0.8) 70%);
+  box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.4);
+  border-radius: 30px;
+  border: none;
+  & {
+    transition: 0.3s cubic-bezier(0.455, 0.03, 0.515, 0.955);
+  }
+  :hover {
+    filter: hue-rotate(15deg) brightness(110%) saturate(110%);
+    box-shadow: 0px 30px 60px rgba(0, 0, 0, 0.7);
+    cursor: pointer;
+    transform: scale(1.1);
+  }
+`;
+
+const CaptchaWrapper = styled.div`
+  display: grid;
+  justify-items: right;
+  gap: 20px;
+  margin: 30px 0;
+`;
---
+++ src/features/form/fields/MyTextInputSim.js
@@ -0,0 +1,76 @@
+import React from "react";
+import styled from "styled-components";
+import { useField } from "formik";
+
+export const MyTextInputSim = ({
+  label,
+  labelClass,
+  br = true,
+  placeholder,
+  ...props
+}) => {
+  const [field, meta] = useField(props);
+  return (
+    <Wrapper>
+      <label className={labelClass}>
+        <TextPlusInputWrapper>
+          <Label>{label}</Label>
+          <InputWrapper>
+            <Input
+              {...field}
+              {...props}
+              placeholder={placeholder}
+              maxlength="256"
+              size="40"
+            />
+          </InputWrapper>
+        </TextPlusInputWrapper>
+
+        {br && <br />}
+      </label>
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  margin: 10px 0;
+`;
+
+const Input = styled.input`
+  font-size: 16px;
+  line-height: 130%;
+  text-align: left;
+  color: rgba(0, 0, 0, 0.8);
+  background: transparent;
+  /* get rid of input border */
+  border: 0;
+  /* no border highlight when typing */
+  outline: none;
+`;
+
+const InputWrapper = styled.div`
+  display: inline;
+  background: rgba(255, 255, 255, 0.8);
+  border-radius: 5px;
+  padding: 5px;
+`;
+
+const TextPlusInputWrapper = styled.div`
+  display: grid;
+  grid-template-columns: minmax(100px, 180px) minmax(200px, 400px);
+  gap: 5px;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  margin: 0px 30px 0px 0px;
+  font-weight: 500;
+  font-size: 17px;
+  line-height: 130%;
+  /* text-transform: uppercase; */
+  color: rgba(255, 255, 255, 0.95);
+  text-shadow: 0px 10px 25px rgba(0, 0, 0, 0.8);
+`;
