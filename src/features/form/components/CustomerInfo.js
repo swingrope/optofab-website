d6d59425217bf395c6606b7a5ab61d130a2a520b
tian <tianwu@outlook.com>
2021-10-07 20:56:19 +1100

    fix typo

--- src/features/form/components/CustomerInfo.js
+++ src/features/form/components/CustomerInfo.js
@@ -12,7 +12,8 @@ export const customerInfoInitialValues = {
 };
 
 export default function CustomerInfo({ part }) {
-  const baseURL = "http://localhost:8080/comp8715/optofab-website/backend/api/OrderRequest";
+  const baseURL =
+    "http://localhost:8080/comp8715/optofab-website/backend/api/OrderRequest";
   //const baseURL = 'http://localhost:8080/comp8715/optofab-website/src/api/OrderRequest.php'
   const [verified, setVerified] = useState(false);
 
